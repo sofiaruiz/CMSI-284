e167c8a69644eba78f1c697ef5f97aa50aa0ecfb
Ray Toal <rtoal@lmu.edu>
2018-05-09 17:05:03 -0700

    Fix typos...now it works

--- homework04/read.c
+++ homework04/read.c
@@ -1,16 +1,17 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdbool.h>
 
 int returnCode = 0;
 
-static bool legal(__int32_t codepoint) { //tells when a codepoint is legal
+static bool legal(uint32_t codepoint) { //tells when a codepoint is legal
     return codepoint <= 0x0010FFFF
         && (codepoint < 0x0000FDD0 || codepoint > 0x0000FDEF)
         && (codepoint & 0x0000FFFE) != 0x0000FFFE;
 }
 
-void to_utf_8(__int32_t c) {
+void to_utf_8(uint32_t c) {
     if (!legal(c)) {
         returnCode = 1;
         putchar(0xEF);
@@ -36,7 +37,7 @@ void to_utf_8(__int32_t c) {
 int main(){
     const int BLOCK_SIZE = 500;
 
-    unit32_t buffer[BLOCK_SIZE];
+    uint32_t buffer[BLOCK_SIZE];
 
     while (true) {
         int size = fread(buffer, 4, BLOCK_SIZE, stdin); //returns the amount of bytes
